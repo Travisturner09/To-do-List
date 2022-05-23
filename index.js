011c9b5b5bccb3d77079b53e158732da9de1fc5e
Travisturner09 <tnadekunle@gmail.com>
2022-05-23 11:53:46 +0100

    to-do list

---
+++ index.js
@@ -0,0 +1,46 @@
+
+var list = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < list.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  list[i].appendChild(span);
+}
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("newInput").value;
+  var t = document.createTextNode(inputValue);
+  var error = document.getElementById("error")
+  li.appendChild(t);
+  if (inputValue === '') {
+      error.textContent = "Please enter a valid title"
+      error.style.color = "red"
+  } else {
+    document.getElementById("newUL").appendChild(li);
+  }
+  document.getElementById("newInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
